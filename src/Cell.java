9ff935994d55bef202b057bc364be1a9560c9cfc
Vaibhav Gupta <vaibhav.gupta@students.mq.edu.au>
2024-08-07 13:59:05 +1000

    Task 6 commit(in class work)

--- src/Cell.java
+++ src/Cell.java
@@ -3,31 +3,27 @@ import java.awt.Graphics;
 import java.awt.Point;
 import java.awt.Rectangle;
 
-public class Cell extends Rectangle {
-    static int size = 35;
+public class Cell extends Rectangle{
 
-    // constructors
-    public Cell(int x, int y, int width, int height) {
-        super(x, y, width, height);
-    }
+  static int size = 35;
 
-    // methods
-    public void paint(Graphics g, Point mousePos) {
-        if (mousePos != null && contains(mousePos)) {
-            g.setColor(Color.GRAY);
-        } else {
-            g.setColor(Color.WHITE);
-        }
-        g.fillRect(x, y, width, height);
-        g.setColor(Color.BLACK);
-        g.drawRect(x, y, width, height);
-    }
+  public Cell(int inX, int inY) {
+    super(inX, inY, size, size);
+  }
 
-    @Override
-    public boolean contains(Point p) {
-        if (p == null) {
-            return false;
-        }
-        return super.contains(p);
+  public void paint(Graphics g, Point mousePos) {
+    g.setColor(contains(mousePos) ? Color.GRAY : Color.WHITE);
+    g.fillRect(x, y, size, size);
+    g.setColor(Color.BLACK);
+    g.drawRect(x, y, size, size);
+  }
+
+  @Override
+  public boolean contains(Point p) {
+    if(p != null) {
+      return super.contains(p);
     }
+    
+    return false;
+  }
 }
